b4e1497e3bd525c7a4e2f88d5a530998e016e9fa
tempet <tempetflamer@gmail.com>
2022-12-12 18:21:56 +0100

    add focus trap filter
    add tab focus
    
    add enter listen
    
    add escape (some bugs after first use)
    
    add key up down right and left nav
    
    need to add a close systemwhen you click on the webpage

--- scripts/utils/filter.js
+++ scripts/utils/filter.js
@@ -1,13 +1,17 @@
 const containerListbox = document.querySelector(".gallery__listbox-container");
+const optionListbox = document.querySelector(".gallery__listbox-options");
+const optionAllListbox = document.querySelectorAll(".gallery__listbox-options > div");
 const containerListboxState = document.querySelector(".gallery__listbox-container--state");
 const select = document.querySelector(".gallery__listbox-container__listbox");
 const galeryListData = document.querySelector(".gallery__list__data");
+const galeryList = document.querySelector(".gallery__list");
 
 let arrayFilter = [];
 let indexFilter = 0;
 
 //By defaut, order by popularity, so popularity option id hidden
-select.children[0].style.display = "none";
+//select.children[0].style.display = "none";
+//select.dataset.selected = "popularité" // pour une raison que je ne comrpend pas mon data select est  à popularité
 
 /**
  * Filter array initialization 
@@ -21,45 +25,173 @@ function initArrayLFilter(title, likes, date, indexFilter) {
   indexFilter = indexFilter + 1;
 }
 
+function resetListbox() {
+  console.log("entére dans resetlistbox");
+// un try catch ne sert à rien car il faut bien les enlever normalement
+  document.querySelector(".listbox-close").removeEventListener("click", resetListbox);
+  document.querySelector(".listbox-close").removeEventListener("keydown", resetListboxKey);
+  //document.querySelector("main").removeEventListener("click", resetListbox);
+/*   document.querySelector(".listbox-close").removeEventListener("keydown", (e) => {
+    if (e.code === "Enter") { resetListbox(); }
+  }); */
+  containerListboxState.classList.remove("listbox-close");
+  galeryList.style.marginTop = "0";
+}
+
+function resetListboxKey(domCloseListbox) {
+  console.log("resetListboxKey");
+  if ( domCloseListbox.code === "Enter") { 
+    console.log("resetListboxKey2"); 
+    resetListbox(); 
+  }
+}
+
+function closeListbox() {
+  resetListbox();
+  optionListbox.classList.toggle("hidden");
+  swapArrow();
+}
+function closeListbox2() {
+  resetListbox();
+  optionListbox.classList.toggle("hidden");
+  swapArrow();
+}
+
+function openListbox() {
+  optionListbox.classList.toggle("hidden");
+  if (select.dataset.selected == "popularité") {
+    console.log("test")
+    optionListbox.children[0].style.display = "none";
+    optionListbox.children[1].style.display = "none";
+
+    optionListbox.children[2].style.display = "block";
+    optionListbox.children[3].style.display = "block";
+    optionListbox.children[4].style.display = "block";
+    optionListbox.children[5].style.display = "block";
+  }
+  else if (select.dataset.selected == "date") {
+    optionListbox.children[2].style.display = "none";
+    optionListbox.children[3].style.display = "none";
+
+    optionListbox.children[0].style.display = "block";
+    optionListbox.children[1].style.display = "block";
+    optionListbox.children[4].style.display = "block";
+    optionListbox.children[5].style.display = "block";
+  }
+  else if (select.dataset.selected == "titre") {
+    optionListbox.children[0].style.display = "block";
+    optionListbox.children[1].style.display = "block";
+    optionListbox.children[2].style.display = "block";
+    optionListbox.children[3].style.display = "block";
+
+    optionListbox.children[4].style.display = "none";
+    optionListbox.children[5].style.display = "none";
+  }
+  swapArrow();
+  //escTrapFocusFilter();
+  //mettre al box en display absolute
+  //galeryList.style.marginTop = "-5.72rem"
+  //galeryList.style.marginTop = "-6.87rem"
+  galeryList.style.marginTop = "-6.95rem"
+  containerListboxState.classList.add("listbox-close");
+  const domCloseListbox = document.querySelector(".listbox-close");
+  //document.querySelector("main").addEventListener("click", closeListbox); // ça marche pas j'arrive pas le remove et ça me fais chiez
+  domCloseListbox.addEventListener("click", resetListbox);
+  domCloseListbox.addEventListener("keydown", resetListboxKey);
+  document.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
+ 
+  trapFocusFilter();
+/*   document.querySelector(".listbox-close").addEventListener("keydown", (e) => {
+    if (e.code === "Enter") { resetListbox(); }
+  }); */
+  //faire passer toute la listbox sur state
+  //ajouter une classe close pour suivre si on quite autre part et de nouveau sur le bouton
+  //y'a un outline en permanence sur la lisbox là
+
+
+
+  /*   
+    let listboxOptionsDesign = document.createElement("div");
+    listboxOptionsDesign.classList.add("gallery__listbox-container__options-design");
+    listboxOptionsDesign.style.marginLeft = '6.75rem'
+    listboxOptionsDesign.style.marginTop = '-1rem'
+    listboxOptionsDesign.style.zIndex = 5;
+    listboxOptionsDesign.style.position= "absolute";
+    containerListbox.after(listboxOptionsDesign);
+    let div = document.createElement("div");
+    div.style.background="#901C1C";
+    div.style.width="9.92rem";
+    div.style.height="7rem";
+    div.style.overflow="hidden"
+    div.style.borderRadius = "0 0 5px 5px"
+    listboxOptionsDesign.appendChild(div);
+    containerListboxState.style.borderRadius = "5px 5px 0 0";
+    // // le border radius est laissé sur select uniquement pour que l'encadré s'affiche bien après accès avec tab ou clique (accssibilité) // en faite on va le mettre en outline none donc on va s'en foutre de l'encadré normalement à tester
+    select.style.outline = "none"; 
+    */
+
+  //aafficher des div à la place des option et les rendres cliquables
+  console.log();
+}
+
 function swapArrow() {
   const arrowUp = document.querySelector(".fa-chevron-up");
   const arrowDown = document.querySelector(".fa-chevron-down");
   arrowUp.classList.toggle("hidden");
   arrowDown.classList.toggle("hidden");
 
-  let listboxOptionsDesign = document.createElement("div");
-  listboxOptionsDesign.classList.add("gallery__listbox-container__options-design");
-  listboxOptionsDesign.style.marginLeft = '6.75rem'
-  listboxOptionsDesign.style.marginTop = '-1rem'
-  listboxOptionsDesign.style.zIndex = 5;
-  listboxOptionsDesign.style.position= "absolute";
-  containerListbox.after(listboxOptionsDesign);
-  let div = document.createElement("div");
-  div.style.background="#901C1C";
-  div.style.width="9.92rem";
-  div.style.height="7rem";
-  div.style.overflow="hidden"
-  div.style.borderRadius = "0 0 5px 5px"
-  listboxOptionsDesign.appendChild(div);
-  containerListboxState.style.borderRadius = "5px 5px 0 0";
-  // // le border radius est laissé sur select uniquement pour que l'encadré s'affiche bien après accès avec tab ou clique (accssibilité) // en faite on va le mettre en outline none donc on va s'en foutre de l'encadré normalement à tester
-  select.style.outline = "none";
-
-  //aafficher des div à la place des option et les rendres cliquables
-  console.log();
 }
 
 function filterBy(e) {
-  const filter = e.currentTarget.value;
+  //console.log("enter in filter", e.dataset.option, e.dataset.option)
+  //e.dataset.option;
+  let filter = '';
+/*   if (e.currentTarget.dataset.option === null){
+    filter = document.activeElement; // aps besoin , jsute besoin s'enlever  current target
+  }
+  else {filter = e.currentTarget.dataset.option;} */
+/*   try {
+    filter = e.currentTarget.dataset.option;
+    
+  } catch {
+    filter = document.activeElement;
+  } */
+  try {
+    filter = e.currentTarget.dataset.option; // click
+    
+  } catch {
+    filter = e.dataset.option; // key
+  }
+
   let arrayStart = arrayFilter;
   let arrayEnd;
   let i = 0;
+  //console.log(select.dataset.selected);
+  closeListbox();
+
+
 
   switch (filter) {
     case "popularité":
-      select.children[0].style.display = "none";
-      select.children[1].style.display = "block";
-      select.children[2].style.display = "block";
+      // Display or hide value selected and separator // pas sur d'avori besoin de metrte ça surement juste besoin de changer data-selected et textcontent comme à=ça se lit à l'ouvertur =e de la modale
+/*       // Popularity
+      optionListbox.children[0].style.display = "none";
+      optionListbox.children[1].style.display = "none";
+      // Date
+      optionListbox.children[2].style.display = "block";
+      optionListbox.children[3].style.display = "block";
+      //Title
+      optionListbox.children[4].style.display = "block";
+      optionListbox.children[5].style.display = "block"; */
+
+      // Changer value selected
+      select.dataset.selected = "popularité";
+      select.textContent = "Popularité";
+
       i = 0;
       arrayEnd = arrayStart.sort((a, b) => b.likes - a.likes);
       arrayEnd.forEach((element) => {
@@ -70,9 +202,10 @@ function filterBy(e) {
       break;
 
     case "date":
-      select.children[0].style.display = "block";
-      select.children[1].style.display = "none";
-      select.children[2].style.display = "block";
+      // Changer value selected
+      select.dataset.selected = "date";
+      select.textContent = "Date";
+
       i = 0;
       arrayEnd = arrayStart.sort((a, b) => new Date(b.date) - new Date(a.date));
       arrayEnd.forEach((element) => {
@@ -83,9 +216,10 @@ function filterBy(e) {
       break;
 
     case "titre":
-      select.children[0].style.display = "block";
-      select.children[1].style.display = "block";
-      select.children[2].style.display = "none";
+      // Changer value selected
+      select.dataset.selected = "titre";
+      select.textContent = "Titre";
+
       i = 0;
       arrayEnd = arrayStart.sort(function (a, b) {
         if (a.title < b.title) {
@@ -104,13 +238,108 @@ function filterBy(e) {
       });
       break;
 
-    default:
+      default:
       break;
   }
 
 
 }
+
+function filterByK(e) {
+  const focusElement = document.activeElement;
+    console.log("enter in filter", focusElement.dataset.option)
+    //e.dataset.option;
+    const filter = focusElement.currentTarget.dataset.option;
+    let arrayStart = arrayFilter;
+    let arrayEnd;
+    let i = 0;
+    //console.log(select.dataset.selected);
+    closeListbox();
+  
+  
+  
+    switch (filter) {
+      case "popularité":
+        // Display or hide value selected and separator // pas sur d'avori besoin de metrte ça surement juste besoin de changer data-selected et textcontent comme à=ça se lit à l'ouvertur =e de la modale
+  /*       // Popularity
+        optionListbox.children[0].style.display = "none";
+        optionListbox.children[1].style.display = "none";
+        // Date
+        optionListbox.children[2].style.display = "block";
+        optionListbox.children[3].style.display = "block";
+        //Title
+        optionListbox.children[4].style.display = "block";
+        optionListbox.children[5].style.display = "block"; */
+  
+        // Changer value selected
+        select.dataset.selected = "popularité";
+        select.textContent = "Popularité";
+  
+        i = 0;
+        arrayEnd = arrayStart.sort((a, b) => b.likes - a.likes);
+        arrayEnd.forEach((element) => {
+          const childData = document.querySelector('.gallery__list__data:nth-child(' + element.index + ')');
+          document.querySelector('.gallery__list__data:nth-child(' + element.index + ')').style.order = i + 1;
+          i = i + 1;
+        });
+        break;
+  
+      case "date":
+        // Changer value selected
+        select.dataset.selected = "date";
+        select.textContent = "Date";
+  
+        i = 0;
+        arrayEnd = arrayStart.sort((a, b) => new Date(b.date) - new Date(a.date));
+        arrayEnd.forEach((element) => {
+          const childData = document.querySelector('.gallery__list__data:nth-child(' + element.index + ')');
+          document.querySelector('.gallery__list__data:nth-child(' + element.index + ')').style.order = i + 1;
+          i = i + 1;
+        });
+        break;
+  
+      case "titre":
+        // Changer value selected
+        select.dataset.selected = "titre";
+        select.textContent = "Titre";
+  
+        i = 0;
+        arrayEnd = arrayStart.sort(function (a, b) {
+          if (a.title < b.title) {
+            return -1;
+          }
+          if (a.title > b.title) {
+            return 1;
+          }
+          return 0;
+        });
+  
+        arrayEnd.forEach((element) => {
+          const childData = document.querySelector('.gallery__list__data:nth-child(' + element.index + ')');
+          childData.style.order = i + 1;
+          i = i + 1;
+        });
+        break;
+  
+        default:
+        break;
+    }
+
+
+}
 //style="display: none;"
 //Gallery filter event listener
-select.addEventListener("change", filterBy);
-select.addEventListener("click", swapArrow);
+//select.addEventListener("change", filterBy);
+containerListboxState.addEventListener("click", openListbox); // il faut rendre toute la zone cliquable avec containerListboxState a la place de select
+//changer le tab index au parent pour permettre la navigation
+containerListboxState.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") { openListbox(); }
+});
+optionAllListbox.forEach((e) => e.addEventListener("click", filterBy));
+/* optionAllListbox.forEach((e) => e.addEventListener("keydown", (k, e) => {
+  if (k.code === "Enter") { filterByK(e); }
+})); */
+//optionAllListbox.forEach((e) => e.addEventListener("keydown", filterByK));
+optionAllListbox.forEach((e) => e.addEventListener("keydown", (k) => {
+  if (k.code === "Enter") { filterBy(e); }
+}));
--- scripts/utils/focusTrap.js
+++ scripts/utils/focusTrap.js
@@ -1,57 +1,257 @@
 function trapFocusContact() {
-    const element = contactModal;
-    const focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
-    const firstFocusableEl = focusableEls[0]; // par defaut il est sur prenom le focus mais on peut aussi mettre le premier élément focusable sur 1 est le dernier sur 0
-    const lastFocusableEl = focusableEls[focusableEls.length - 1]; 
-    const KEYCODE_TAB = 9;
-  
-    element.addEventListener('keydown', function(e) {
-        const isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
-  
-      if (!isTabPressed) { 
-        return; 
+  const element = contactModal;
+  const focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+  const firstFocusableEl = focusableEls[0]; // par defaut il est sur prenom le focus mais on peut aussi mettre le premier élément focusable sur 1 est le dernier sur 0
+  const lastFocusableEl = focusableEls[focusableEls.length - 1];
+  const KEYCODE_TAB = 9;
+
+  element.addEventListener('keydown', function (e) {
+    const isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+    if (!isTabPressed) {
+      return;
+    }
+
+    if (e.shiftKey) /* shift + tab */ {
+      if (document.activeElement === firstFocusableEl) {
+        lastFocusableEl.focus();
+        e.preventDefault();
       }
-  
-      if ( e.shiftKey ) /* shift + tab */ {
+    } else /* tab */ {
+      if (document.activeElement === lastFocusableEl) {
+        firstFocusableEl.focus();
+        e.preventDefault();
+      }
+    }
+  });
+}
+
+function trapFocusLightbox() {
+  const element = lightboxModal;
+  const focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+  const firstFocusableEl = focusableEls[0];
+  const lastFocusableEl = focusableEls[focusableEls.length - 1];
+  const nextFocusableEl = focusableEls[0 + 1];
+  const prevFocusableEl = focusableEls[0 - 1];
+  const KEYCODE_TAB = 9;
+
+  element.addEventListener('keydown', function (e) {
+    const isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+    if (!isTabPressed) {
+      return;
+    }
+
+    if (e.shiftKey) /* shift + tab */ {
+      if (document.activeElement === btnPrevLightbox) {
+        btnCloseLightbox.focus();
+        e.preventDefault();
+      }
+    } else /* tab */ {
+      if (document.activeElement === btnCloseLightbox) {
+        btnPrevLightbox.focus();
+        e.preventDefault();
+      }
+    }
+  });
+}
+
+function trapFocusFilter() {
+  const element = containerListbox;
+  const focusableEls = element.querySelectorAll('div:not([disabled])');
+  const firstFocusableEl = focusableEls[0];
+  const lastFocusableEl = focusableEls[focusableEls.length - 1];
+  const nextFocusableEl = focusableEls[0 + 1];
+  const prevFocusableEl = focusableEls[0 - 1];
+  const KEYCODE_TAB = 9;
+
+  element.addEventListener('keydown', function (e) {
+    // 37 = left arrow & 39 = right arrow & 27 = esc key & 38 = up arrow & 40 = down arrow
+    const isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB || e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 27/*  || e.keyCode === 38 || e.keyCode === 40 */);
+
+    if (!isTabPressed) {
+      return;
+    }
+    // left, right arrow and esc key
+    if (e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 27) {
+      closeListbox();
+      //e.preventDefault();
+    }
+    else {
+      if (e.shiftKey/*  || e.keyCode === 38 */) /* shift + tab */ {
         if (document.activeElement === firstFocusableEl) {
-            lastFocusableEl.focus();
-            e.preventDefault();
-          }
-        } else /* tab */ {
-        if (document.activeElement === lastFocusableEl) {
+          lastFocusableEl.focus();
+          e.preventDefault();
+        }
+      } else /* tab */ {
+        if (e.key === 'Tab'/*  || e.keyCode === 40 */) {
+          if (document.activeElement === lastFocusableEl) {
             firstFocusableEl.focus();
             e.preventDefault();
           }
         }
-    });
-  }
-
-  function trapFocusLightbox() {
-    const element = lightboxModal;
-    const focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
-    const firstFocusableEl = focusableEls[0]; 
-    const lastFocusableEl = focusableEls[focusableEls.length - 1]; 
-    const nextFocusableEl = focusableEls[0+1];  
-    const prevFocusableEl = focusableEls[0-1];  
-    const KEYCODE_TAB = 9;
-  
-    element.addEventListener('keydown', function(e) {
-        const isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
-  
-      if (!isTabPressed) { 
-        return; 
       }
-  
-      if ( e.shiftKey ) /* shift + tab */ {
-        if (document.activeElement === btnPrevLightbox) {
-            btnCloseLightbox.focus();
-            e.preventDefault();
-          }
-        } else /* tab */ {
-        if (document.activeElement === btnCloseLightbox) {
-            btnPrevLightbox.focus();
-            e.preventDefault();
-          }
+    }
+
+    /*     const isArrowPressed = (e.key === 'ArrowDown' || e.key === 'ArrowUp');
+    
+        if (!isArrowPressed) {
+          return;
         }
-    });
-  }
+    
+        if (e.key === 'ArrowUp') {
+          if (document.activeElement === firstFocusableEl) {
+            lastFocusableEl.focus();
+            e.preventDefault();
+          }
+        } else {
+          if (e.key === 'ArrowDown') {
+            if (document.activeElement === lastFocusableEl) {
+              firstFocusableEl.focus();
+              e.preventDefault();
+            }
+          }
+        } */
+
+    /*     const isArrowPressedCode = (e.keyCode === 38 || e.keyCode === 40);
+    
+        if (!isArrowPressedCode) {
+          return;
+        }
+    
+        if (e.keyCode === 38) {
+          if (document.activeElement === firstFocusableEl) {
+            lastFocusableEl.focus();
+            e.preventDefault();
+          }
+        } else {
+          if (e.keyCode === 40) {
+            if (document.activeElement === lastFocusableEl) {
+              firstFocusableEl.focus();
+              e.preventDefault();
+            }
+          }
+        } */
+
+    /* 
+        const isArrowPressedCode = (e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 27); // left, right arrow and esc key
+    
+        if (e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 27) {
+          console.log('test')
+          closeListbox();
+        }
+    
+     */
+
+  });
+}
+
+function trapFocusFilter2() {
+  //faut que je trouve un moyen d'enlever les div qui sont pas utile de l'équation
+  const element = containerListbox;
+  const focusableEls = element.querySelectorAll('div[tabindex="0"]:not([disabled])' );
+  const firstFocusableEl = focusableEls[0];
+  const lastFocusableEl = focusableEls[focusableEls.length - 1];
+  const nextFocusableEl = focusableEls[0 + 1];
+  const prevFocusableEl = focusableEls[0 - 1];
+  const KEYCODE_TAB = 9;
+  let actuelFocus = 0;
+
+  element.addEventListener('keydown', function (e) {
+    // 37 = left arrow & 39 = right arrow & 27 = esc key & 38 = up arrow & 40 = down arrow
+    const isTabPressed = (e.keyCode === 38 || e.keyCode === 40);
+
+    if (!isTabPressed) {
+      return;
+    }
+
+    
+// if (e.keyCode === 38) /* shift + tab */ {
+    if (e.keyCode === 38) /* shift + tab */ {
+      if (document.activeElement === firstFocusableEl) {
+        actuelFocus = focusableEls.length - 1;
+        lastFocusableEl.focus();
+        e.preventDefault();
+      }
+      else {
+        actuelFocus = actuelFocus - 1;
+        focusableEls[actuelFocus].focus();
+      }
+    } else /* tab */ {
+      if (e.keyCode === 40) {
+        if (document.activeElement === lastFocusableEl) {
+          actuelFocus = 0
+          firstFocusableEl.focus();
+          e.preventDefault();
+        }
+        else {
+          actuelFocus = actuelFocus + 1;
+          focusableEls[actuelFocus].focus();
+        }
+      }
+    }
+
+
+    /*     const isArrowPressed = (e.key === 'ArrowDown' || e.key === 'ArrowUp');
+    
+        if (!isArrowPressed) {
+          return;
+        }
+    
+        if (e.key === 'ArrowUp') {
+          if (document.activeElement === firstFocusableEl) {
+            lastFocusableEl.focus();
+            e.preventDefault();
+          }
+        } else {
+          if (e.key === 'ArrowDown') {
+            if (document.activeElement === lastFocusableEl) {
+              firstFocusableEl.focus();
+              e.preventDefault();
+            }
+          }
+        } */
+
+    /*     const isArrowPressedCode = (e.keyCode === 38 || e.keyCode === 40);
+    
+        if (!isArrowPressedCode) {
+          return;
+        }
+    
+        if (e.keyCode === 38) {
+          if (document.activeElement === firstFocusableEl) {
+            lastFocusableEl.focus();
+            e.preventDefault();
+          }
+        } else {
+          if (e.keyCode === 40) {
+            if (document.activeElement === lastFocusableEl) {
+              firstFocusableEl.focus();
+              e.preventDefault();
+            }
+          }
+        } */
+
+    /* 
+        const isArrowPressedCode = (e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 27); // left, right arrow and esc key
+    
+        if (e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 27) {
+          console.log('test')
+          closeListbox();
+        }
+    
+     */
+
+  });
+}
+
+/* function escTrapFocusFilter() {
+  const element2 = containerListbox;
+
+  element2.addEventListener('keydown', function (e) {
+    // left, right arrow and esc key
+    if (e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 27) {
+      closeListbox();
+    }
+  });
+} */
