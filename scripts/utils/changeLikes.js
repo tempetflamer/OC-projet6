c115712b510b7b9056a9cf3b30ff6db0a69f57fc
tempet <tempetflamer@gmail.com>
2022-12-28 12:57:28 +0100

    fix overload to change an element

--- scripts/utils/changeLikes.js
+++ scripts/utils/changeLikes.js
@@ -6,23 +6,37 @@
  */
 function changelikes(icon, number, mediaNum) {
     const totaLikes = document.querySelector(".stats__likes__number");
-    if (icon.classList.contains("heart--empty")){
-        number.textContent = parseInt(number.textContent) + 1; 
+    if (icon.classList.contains("heart--empty")) {
+        number.textContent = parseInt(number.textContent) + 1;
         totaLikes.textContent = parseInt(totaLikes.textContent) + 1;
         icon.classList.remove("heart--empty");
         icon.classList.remove("fa-regular");
         icon.classList.add("heart--filled");
-        icon.classList.add("fa-solid");   
-        arrayMedia.map(function(media) {if (media.index === mediaNum) { media.liked = true} });
+        icon.classList.add("fa-solid");
+        arrayMedia = arrayMedia.map(function (media) {
+            if (media.index === mediaNum) { 
+                return { ...media, liked: true } 
+            } 
+            else { 
+                return media 
+            } 
+        })
     }
     else {
         number.textContent = parseInt(number.textContent) - 1;
         totaLikes.textContent = parseInt(totaLikes.textContent) - 1;
         icon.classList.remove("heart--filled");
         icon.classList.remove("fa-solid");
-        icon.classList.add("heart--empty");  
+        icon.classList.add("heart--empty");
         icon.classList.add("fa-regular");
-        arrayMedia.map(function(media) {if (media.index === mediaNum) { media.liked = false} });
+        arrayMedia = arrayMedia.map(function (media) {
+            if (media.index === mediaNum) { 
+                return { ...media, liked: false } 
+            } 
+            else { 
+                return media 
+            } 
+        })
     }
 
 }
